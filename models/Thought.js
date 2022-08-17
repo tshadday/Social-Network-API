d96f3b16dfc30855552da44838b27ed16ef63969
Tanner Shadday <tshadday99@gmail.com>
2022-08-17 11:56:18 -0400

    created models schemas and removed Reaction model

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions:[reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query
+
+const User = model('user', userSchema);
+
+module.exports = User;
